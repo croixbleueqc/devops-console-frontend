0cd4c293968ca763d191921180a4806bf05329ed
Guillaume ANTECH <guillaume.antech@qc.croixbleue.ca>
2022-04-06 16:14:46 -0400

    ADD test github action bit tagging and netlify app deployement

--- devops/netlify/react-app/app.tsx
+++ devops/netlify/react-app/app.tsx
@@ -5,6 +5,7 @@ export function App() {
     return (
         <div className={styles.container}>
             <div className={styles.card}>
+                <h1>TEST</h1>
                 <p>This React app component is deployed to Netlify</p>
                 <p>It represents an example of devops-console implementation</p>
             </div>
