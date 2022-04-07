b1e09053c94d92dd65832c1f86dc13cae6086e61
Guillaume ANTECH <guillaume.antech@qc.croixbleue.ca>
2022-04-06 23:15:38 -0400

    Test soft tag

--- devops/netlify/react-app/app.tsx
+++ devops/netlify/react-app/app.tsx
@@ -5,7 +5,7 @@ export function App() {
     return (
         <div className={styles.container}>
             <div className={styles.card}>
-                <p>This React app component is deployed to Netlify</p>
+                <p>This react app component is deployed to Netlify</p>
                 <p>It represents an example of devops-console implementation</p>
             </div>
         </div>
