025163445c3eede82e610edd1e1a90366dbd120d
Guillaume ANTECH <guillaume.antech@qc.croixbleue.ca>
2022-11-11 14:43:30 -0500

    FIX eslint config for vscode FIX prettier onSave format in vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-    extends: ['@teambit/react.eslint-config-bit-react'],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
+  extends: ['./node_modules/@teambit/react.eslint-config-bit-react'],
+  rules: {
+    'import/order': 'off', // Avoid VSCode eslint error: `The "path" argument must be of type string. Received null Occurred while linting`
+  },
+  parserOptions: {
+    project: ['./tsconfig.json'],
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,6 @@ const baseConfig = require('@teambit/react/prettier/prettier.config');
 module.exports = {
     ...baseConfig,
     endOfLine: 'lf',
-    tabWidth: 4,
+    // tabWidth: 4, // Sadly, `bit lint` command does not use root prettier/eslint config. So to avoid formatting conflict, `tabWidth` cannot be define here because it already define in bit prettier config: node_modules/@teambit/react/prettier/prettier.config.js
     printWidth: 120,
 };
